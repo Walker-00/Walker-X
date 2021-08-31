932eec277105c7eb5546474cd77cd7a72a8caa5a
Ŵâĺkêř <85013114+Walker-00@users.noreply.github.com>
2021-08-31 10:49:06 +0630

    Add files via upload

---
+++ Walker-X.py
@@ -0,0 +1,37 @@
+import os
+
+os.system("clear")
+print( """
+#		     #		  $	  $
+ #		    #		   $	   $
+  #		   #		    $$$$$$$$$
+   #		  #		     $	     $
+    #     ##     #        ###$$$      $	      $
+     #   #  #   #		       $$$$$$$$$
+      # #    # #			$       $
+       #      #				 $       $
+""")
+print( """
+########################$$$$$$$$$$$$$$$$$$$$$$$$$$
+#		1.          cute	         $
+#		2.          hentai	         $
+#		3.          family	         $
+#		4.          teen	         $
+#		5.          japan	         $
+#		6.          china	         $
+########################$$$$$$$$$$$$$$$$$$$$$$$$$$
+""")
+
+x = input("Walker-X@localhost# ")
+while x == "1" :
+      os.system("termux-open https://www.xnxx.com/search/cute")
+while x == "2" :
+      os.system("termux-open https://www.xnxx.com/search/hentai")
+while x == "3" :
+      os.system("termux-open https://www.xnxx.com/search/family")
+while x == "4" :
+      os.system("termux-open https://www.xnxx.com/search/teen")
+while x == "5" :
+      os.system("termux-open https://www.xnxx.com/search/japan")
+while x == "6" :
+      os.system("termux-open https://www.xnxx.com/search/china")
---
+++ Walker-X.sh
@@ -0,0 +1,50 @@
+clear
+
+printf "#		 #	   #     #"
+echo
+printf " #		#	    #     #"
+echo
+printf "  #	       #	     #######"
+echo
+printf "   #	      #     ######    #     #"
+echo
+printf "    #   ##   #		       #######"
+echo
+printf "     # #  # #			#     #"
+echo
+printf "      #    #			 #     #"
+echo
+printf "    ##########################################"
+echo
+printf "    #       1.                 cute          #"
+echo
+printf "    #       2.                 hentai        #"			
+echo
+printf "    #       3.                 family        #"
+echo
+printf "    #       4.                 teen          #"
+echo
+printf "    #       5.                 japan         #"
+echo
+printf "    #       6.                 china         #"
+echo
+printf "    ##########################################"
+echo
+echo
+echo
+
+read -p "Walker-X@localhost# "
+
+if [[ "$REPLY" == 1 ]]; then
+   termux-open https://www.xnxx.com/search/cute
+elif [[ "$REPLY" == 2 ]]; then
+     termux-open https://www.xnxx.com/search/hentai
+elif [[ "$REPLY" == 3 ]]; then
+     termux-open https://www.xnxx.com/search/family
+elif [[ "$REPLY" == 4 ]]; then
+     termux-open https://www.xnxx.com/search/teen
+elif [[ "$REPLY" == 5 ]]; then
+     termux-open https://www.xnxx.com/search/japan
+elif [[ "$REPLY" == 6 ]]; then
+     termux-open https://www.xnxx.com/search/china
+fi
